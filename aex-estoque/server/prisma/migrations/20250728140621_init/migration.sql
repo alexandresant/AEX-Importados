168f1f1852a37c1bdeb26f703c9c2301cac2442c
Alexandre Buhrer <alexandre_sant1@hotmail.com>
2025-08-01 17:24:55 -0300

    feat: Redesigned database

--- aex-estoque/server/prisma/migrations/20250728140621_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Usuario_codigo_key";
---
+++ aex-estoque/server/prisma/migrations/20250801202229_init/migration.sql
@@ -0,0 +1,72 @@
+-- CreateEnum
+CREATE TYPE "TipoMovimentacao" AS ENUM ('ENTRADA', 'SAIDA');
+
+-- CreateEnum
+CREATE TYPE "MotivoEntrada" AS ENUM ('COMPRA', 'DEVOLUCAO', 'AJUSTE_ESTOQUE', 'OUTROS');
+
+-- CreateEnum
+CREATE TYPE "MotivoSaida" AS ENUM ('VENDA', 'CONSUMO', 'TROCA', 'PERDA_AVARIA', 'AJUSTE_ESTOQUE', 'OUTROS');
+
+-- CreateTable
+CREATE TABLE "Produto" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "codigo" TEXT NOT NULL,
+    "preco" DOUBLE PRECISION NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "categoria" TEXT NOT NULL,
+    "fornecedorId" INTEGER,
+
+    CONSTRAINT "Produto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Fornecedor" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "codigo" TEXT NOT NULL,
+    "contato" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Fornecedor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Estoque" (
+    "id" SERIAL NOT NULL,
+    "produtoId" INTEGER NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "tipoMovimentacao" "TipoMovimentacao" NOT NULL,
+    "motivoEntrada" "MotivoEntrada",
+    "motivoSaida" "MotivoSaida",
+    "dataMovimentacao" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "usuarioId" INTEGER,
+
+    CONSTRAINT "Estoque_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Produto_codigo_key" ON "Produto"("codigo");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Fornecedor_codigo_key" ON "Fornecedor"("codigo");
+
+-- AddForeignKey
+ALTER TABLE "Produto" ADD CONSTRAINT "Produto_fornecedorId_fkey" FOREIGN KEY ("fornecedorId") REFERENCES "Fornecedor"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Estoque" ADD CONSTRAINT "Estoque_produtoId_fkey" FOREIGN KEY ("produtoId") REFERENCES "Produto"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Estoque" ADD CONSTRAINT "Estoque_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "Usuario"("id") ON DELETE SET NULL ON UPDATE CASCADE;
