8682435d9db63ee9ef8d54ea1fc4b70d75484b1b
Alexandre Buhrer <alexandre_sant1@hotmail.com>
2025-08-04 16:35:43 -0300

    feat: redesign product page and modals

--- aex-estoque/web/src/pages/produtos/ModalEditarProduto.tsx
+++ aex-estoque/web/src/pages/produtos/ModalEditarProduto.tsx
@@ -1,54 +1,75 @@
+import { DialogTitle } from "@radix-ui/react-dialog"
 import { Button } from "../../components/ui/button"
+import { Dialog, DialogContent, DialogHeader } from "../../components/ui/dialog"
 import { Input } from "../../components/ui/input"
+import { Select, SelectGroup, SelectTrigger, SelectContent, SelectValue, SelectItem } from "../../components/ui/select"
+import { Label } from "../../components/ui/label"
 
 export function ShowModalEditar(props){
     console.log(props.value)
     return(
-        <div className="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center z-50 px-2">
-            <div className="bg-white rounded p-4 w-full max-w-md mx-2">
-                <h2 className="text-xl font-bold mb-4">
-                    Ajustar Estoque: {props.nomeEdit}
-                </h2>
-
-                <Input 
-                    type="text" 
-                    placeholder="Nome"
-                    className="w-full px-3 py-2 mb-4"
-                    value={props.nomeEdit}
-                    onChange={(e) => props.setNomeEdit(e.target.value)}
-                />
-                <Input 
-                    type="text"
-                    placeholder="Preço"
-                    className="w-full px-3 py-2 mb-4"
-                    value={props.precoEdit}
-                    onChange={(e) => props.setPrecoEdit(e.target.value)}
-                />
-                <Input
-                    type="number"
-                    min={1}
-                    placeholder="Quantidade"
-                    className="w-full border px-3 py-2 rounded mb-4"
-                    value={props.quantEdit}
-                    onChange={(e) => props.setQuantEdit(e.target.value)}
-                />
-                <select
-                    className=""
-                    value={props.categoriaEdit}
-                    onChange={(e) => props.setCategoriaEdit(e.target.value)}
+        <div>
+            <Dialog 
+              open={props.open}
+              onOpenChange={props.openChange}  
+            >
+                <DialogContent
+                    onOpenAutoFocus={(e) => e.preventDefault()}
+                    className="sm:max-w-[425px]"
                 >
-                    <option value="">Selecione uma Categoria</option>
-                    {props.categorias.map((categoria) =>(
-                        <option
-                            key={categoria}
-                            value={categoria}
-                        >
-                            {categoria}
-                        </option>
-                    ))}
-                    <option value="nova"> + Nova Categoria</option>
-                </select> 
+                    <DialogHeader>
+                        <DialogTitle>Ajustar Estoque: {props.nomeEdit}</DialogTitle>
+                    </DialogHeader>
+                    <Label>Nome</Label>
+                    <Input 
+                        type="text" 
+                        placeholder="Nome"
+                        className="w-full px-3 py-2"
+                        value={props.nomeEdit}
+                        onChange={(e) => props.setNomeEdit(e.target.value)}
+                    />
 
+                    <Label>Preço</Label>
+                    <Input 
+                        type="text"
+                        placeholder="Preço"
+                        className="w-full px-3 py-2"
+                        value={props.precoEdit}
+                        onChange={(e) => props.setPrecoEdit(e.target.value)}
+                    />
+
+                    <Label>Quantidade</Label>
+                    <Input
+                        type="number"
+                        min={1}
+                        placeholder="Quantidade"
+                        className="w-full border px-3 py-2"
+                        value={props.quantEdit}
+                        onChange={(e) => props.setQuantEdit(e.target.value)}
+                    />
+
+                    <Label>Categoria</Label>
+                    <Select
+                        value={props.categoriaEdit}
+                        onValueChange={(e) => props.setCategoriaEdit(e)}
+                    >
+                        <SelectTrigger>
+                            <SelectValue  className="Selecione uma categoria"/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                {props.categorias.map((cat) => (
+                                    <SelectItem
+                                        key={cat}
+                                        value={cat}
+                                    >
+                                        {cat}
+                                    </SelectItem>
+                                ))}
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+               
                 {props.categoriaEdit === 'nova' && (
                     <input
                         type="text"
@@ -59,28 +80,25 @@ export function ShowModalEditar(props){
                     />
                 )}   
 
-                <div className="flex justify-end space-x-2">
-                    <Button
-                        onClick={props.onCancel}
-                        className="px-4 py-2 rounded border bg-gray-300 text-black hover:bg-gray-400"
-                    >
-                        Cancelar
-                    </Button>
-                    <Button
-                        onClick={props.onConfirm}
-                        className="px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700"
-                    >
-                        Adicionar
-                    </Button>
-                    <Button
-                        onClick={props.onClick}
-                        
-                        className="px-4 py-2 rounded bg-red-600 text-white hover:bg-red-700"
-                    >
-                        Excluir
-                    </Button>
-                </div>
-            </div>
+                    <div className="mt-2 flex justify-end space-x-2">
+                        <Button
+                            onClick={props.onConfirm}
+                            className="px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700"
+                        >
+                            Adicionar
+                        </Button>
+                        <Button
+                            onClick={props.onClick}
+                            
+                            className="px-4 py-2 rounded bg-red-600 text-white hover:bg-red-700"
+                        >
+                            Excluir
+                        </Button>
+                    </div>
+                </DialogContent>
+
+                
+            </Dialog>
         </div>
     )
 }
